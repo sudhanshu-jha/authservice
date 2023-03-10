ae2aaf7f6f199ec7a9cb74dc017819ccffce152e
Sudhanshu Jha <jha.sudhanshu1991@gmail.com>
2023-03-10 11:46:21 +0530

    Update settings.py

--- authservice/settings.py
+++ authservice/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'authservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': os.environ['MYSQL_HOST'],
+        'PORT': os.environ['MYSQL_PORT'],
+        'NAME': os.environ['MYSQL_DATABASE'],
+        'USER': os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
     }
 }
+
 
 
 # Password validation
